3b826ae9e0e82ba84a72f8a3de77f145a349f35c
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-01 16:16:01 +0000

    included POST

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST */
+
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
