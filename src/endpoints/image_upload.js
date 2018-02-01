c0949e2e7b6a2b00454c38f71c9f98d5da8f3e88
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-01 20:17:44 +0000

    image_upload is complete

--- src/endpoints/image_upload.js
+++ src/endpoints/image_upload.js
@@ -1,25 +1,35 @@
 const router = require('express').Router();
 var multer = require('multer');
 var storage = multer.diskStorage({
-  destination:  __dirname + '/puzzle_images/uploads',
+  destination: function (req, file, cb) {
+        cb(null, __dirname + '/puzzle_images/uploads')
+    },
   filename: function(req, file, cb){
-    cb(null, file.filename + '-' + Date.now() + path.extname(file.orignalname));
+    console.log(file.mimetype);
+    cb(null, file.originalname + '-' + Date.now() + getExtension(file));
   }
 });
 
+function getExtension(file) {
+    var res = '';
+    if (file.mimetype === 'image/jpeg') res = '.jpg';
+    if (file.mimetype === 'image/png') res = '.png';
+    return res;
+}
+
 var upload = multer({storage : storage}).single('puzzle');
+
 module.exports = router;
 
 router.post('/', (req, res, next) => {
   upload(req, res, function(err){
     if(err) {
-      return res.end("Error uploading file.")
-    } else if (!req.file) {
-      res.write('No file recieved!');
-      return res.end({success: false});
+      res.status(err.status || 500).json({ "error": { "status_code": err.status || 500, "message": err.code } });
+      console.log("error: " + err.code);
+      return;
     } else {
-      res.write('File recieved.');
-      return res.end({success: true})
+      console.log("file recieved: " + Date.now());
+      return res.end('File received.');
     }
   });
 });
