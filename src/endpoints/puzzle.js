a987d2c48f5c589347fad09ef4c25ca651916a7d
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-14 19:34:12 +0000

    API to check status of puzzle solving

---
+++ src/endpoints/puzzle.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+module.exports = router;
+
+router.post('/check', (req, res, next) => {
+  const hash = req.body.hash;
+  console.log(hash);
+  if (!/^[0-9A-F]+$/i.test(hash)) {
+    return res.json({msg: 'error! puzzle not found'})
+  }
+  let file = process.env.UPLOAD_DIR + '/' + hash
+  if (!fs.existsSync(file)) {
+    return res.json({msg: 'error! puzzle not found'})
+  }
+  pieces_array = [];
+  solutions_array =[];
+  let test = file + '/solving';
+  if (!fs.existsSync(test)) {
+    res.json({solving: false, pieces: pieces_array, solutions: solutions_array});
+  } else {
+    res.json({solving: true});
+  }
+});
+
+/* TODO when solver complete
+
+*/
