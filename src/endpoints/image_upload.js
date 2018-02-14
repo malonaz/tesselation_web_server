48f92053e0f2aba0767199a128217bea1921d82f
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-14 17:50:24 +0000

    fixed filefilter to only accept .jpeg file

--- src/endpoints/image_upload.js
+++ src/endpoints/image_upload.js
@@ -11,29 +11,31 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, os.tmpdir());
   },
-  fileFilter: function (req, file, cb) {
-     if (file.mimetype !== 'image/jpg') {
-       console.log('file type error');
-       return cb(null, false, new Error('File Upload Error'));
-     }
-   },
   filename: function(req, file, cb){
     let buf = crypto.randomBytes(32);
     let temp_name = buf.toString('hex');
     cb(null, temp_name + '.jpg');
-  }
+  },
 });
 
-const upload = multer({storage : storage}).single('puzzle');
+function fileFilter (req, file, cb) {
+  console.log(file.mimetype);
+  if (file.mimetype !== 'image/jpeg') {
+     cb(new Error('File Upload Error'), false);
+   }
+   cb(null, true);
+}
+
+const upload = multer({storage : storage, fileFilter: fileFilter}).single('puzzle');
 
 function moveFileHashedRename(hash, old_filename) {
   console.log(hash);
   console.log(old_filename);
   const new_filename = 'photo.jpg';
-  const dir = __dirname + '/puzzle_images/uploads/' + hash;
+  const dir = process.env.UPLOAD_DIR + '/' + hash + '/';
   if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
-    fs.rename(old_filename, dir + '/' + new_filename);
+    fs.rename(old_filename, dir + new_filename);
   }
   else {
     console.log('Puzzle Folder Already Exists');
@@ -47,16 +49,16 @@ router.post('/', (req, res, next) => {
       console.log("error: " + err.code);
       return;
     } else if (!req.file) {
-      console.log("File Upload Error")
-      return res.end('File Upload Error')
+      return res.json({msg:'File Upload Error'})
     } else{
       const file = os.tmpdir() + '/' + req.file.filename;
       hashFile(file)
         .then((hash) => {
-        moveFileHashedRename(hash, file);
+          moveFileHashedRename(hash, file);
+          res.json({
+            hash: hash
+          })
         });
-      console.log("File uploaded!" );
-      return res.end('File uploaded!');
     }
   });
 });
