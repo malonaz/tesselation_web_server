2afcc48c8992fb6f53f4b92f8c5c9938c431ded1
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-25 22:16:02 +0000

    update hash

--- src/lib/hashFile.js
+++ src/lib/hashFile.js
@@ -1,9 +1,9 @@
 const crypto = require('crypto');
 const fs = require('fs');
-const hash = crypto.createHash('sha256').setEncoding('hex');
 
 module.exports = function hashFile(filename) {
   return new Promise((resolve, reject) => { // async process, need promise to ensure that the file read and hash are completed before returning the hash
+    const hash = crypto.createHash('sha256').setEncoding('hex');
     const input = fs.createReadStream(filename);
     input
       .pipe(hash)
