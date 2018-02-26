de00fe9b78296b7379c9e9513ef2bd5beb7329d1
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-26 00:39:42 +0000

    removed console logs

--- src/endpoints/puzzle.js
+++ src/endpoints/puzzle.js
@@ -5,14 +5,11 @@ module.exports = router;
 
 router.post('/check', (req, res, next) => {
   const hash = req.body.hash;
-  console.log(hash);
   if (!/^[0-9A-F]+$/i.test(hash)) {
-    console.log('not a hash');
     return res.json({msg: 'error! puzzle not found'})
   }
   let file = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + hash
   if (!fs.existsSync(file)) {
-    console.log(file);
     return res.json({msg: 'error! puzzle not found'})
   }
   pieces_array = [];
