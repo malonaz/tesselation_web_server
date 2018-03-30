9d10e102d568416e8345bf6a1df64e22ce7dafd0
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-30 16:49:49 +0100

    fix test issues in image.test

--- test/endpoints/image.test.js
+++ test/endpoints/image.test.js
@@ -3,7 +3,6 @@ const chai = require('chai');
 const fs = require('fs')
 const fse = require('fs-extra')
 const chaiHttp = require('chai-http');
-const should = chai.should();
 const expect = chai.expect;
 const server = require('../server');
 
@@ -14,78 +13,87 @@ const TEST_HASH_UPLOAD = '67affb51d7ea5c6a9a6bd0d86e3008b3377f70c15a8ebc7d6bf6cc
 
 chai.use(chaiHttp);
 
-describe('image/upload', () => {
+describe('POST image/upload', () => {
 
-  describe('POST / : no upload file', (done) => {
+  describe('no upload file', () => {
     // no upload file
-    it('should check and return {msg: error! puzzle not found}', () => {
+    it('should check and return {msg: error! puzzle not found}', (done) => {
       let file = process.env.PRJ_DIR + process.env.WEB_TEST_DIR + '/test.txt';
       chai.request(server)
         .post('/image/upload')
         .attach('puzzle', fs.readFileSync(file), 'test_upload_text.txt')
         .end((err, res) => {
-          res.should.hav.status(500);
-          res.body.should.be.json;
-          res.body.should.have.property('error');
-          res.body.error.should.be.equals('error! puzzle not found');
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(500);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('msg');
+          expect(res.body.msg).to.be.equals('File Upload Error');
           done();
         });
     });
   });
 
-  describe('POST / : upload is empty', (done) => {
+  describe('upload is empty', (done) => {
     // null upload
-    it('should check and return {msg: File Upload Error}', () => {
+    it('should check and return {msg: File Upload Error}', (done) => {
       chai.request(server)
         .post('/image/upload')
         .send('puzzle', '')
         .set('content-type', 'image/jpeg')
         .end((err, res) => {
-          res.should.hav.status(200);
-          res.body.should.be.json;
-          res.body.should.have.property('msg');
-          res.body.msg.should.be.equals('File Upload Error');
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(500);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('msg');
+          expect(res.body.msg).to.be.equals('File Upload Error');
           done();
         });
     });
   });
 
-  describe('POST / : upload is a new picture', (done) => {
+  describe('upload is a new picture', () => {
     // new picture
     // delete folder before test
-    let folder = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + TEST_HASH_UPLOAD;
-    if (fse.exists(folder)) {
-      console.log('path exists');
-      fse.remove(folder, (err) =>{
-        if (err) throw err;
-        console.log(folder + ' deleted before testing....');
-      });
-    }
-    it('should check the hash and return {hash: 67aff....}', () => {
+    before((done) => {
+      let folder = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + TEST_HASH_UPLOAD;
+      if (fse.exists(folder)) {
+        console.log('path exists');
+        fse.remove(folder, (err) =>{
+          if (err) throw err;
+          console.log(folder + ' deleted before testing....');
+          done();
+        });
+      }
+    });
+    it('should check the hash and return {hash: 67aff....}', (done) => {
       let file = process.env.PRJ_DIR + process.env.WEB_TEST_DIR + '/upload_test.jpeg';
       chai.request(server)
         .post('/image/upload')
         .attach('puzzle', fs.readFileSync(file), 'test_upload_image.jpg')
         .end((err, res) => {
-          res.body.should.be.json;
-          res.body.should.have.property('hash');
-          res.body.hash.should.be.equals(TEST_HASH_UPLOAD);
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(200);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('hash');
+          expect(res.body.msg).to.be.equals(TEST_HASH_UPLOAD);
           done();
         });
     });
   });
 
-  describe('POST / : upload pre-existing picture', (done) => {
+  describe('upload pre-existing picture', () => {
     // picture already exists
-    it('should check the hash and return {hash: hash}', () => {
+    it('should check the hash and return {hash: hash}', (done) => {
       let file = process.env.PRJ_DIR + process.env.WEB_TEST_DIR + '/test2.jpg';
       chai.request(server)
         .post('/image/upload')
         .attach('puzzle', fs.readFileSync(file), 'test_upload_image_exist.jpg')
         .end((err, res) => {
-          res.body.should.be.json;
-          res.body.should.have.property('hash');
-          res.body.hash.should.be.equals(TEST_HASH_SOLVED);
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(200);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('hash');
+          expect(res.body.msg).to.be.equals(TEST_HASH_SOLVED);
         });
       });
     });
