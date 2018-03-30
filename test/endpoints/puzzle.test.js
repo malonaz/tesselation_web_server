e73e6cd3f4663d4a4a1cbc40538a34ff7f8d8361
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-30 17:02:18 +0100

    fixes for puzzle.test

--- test/endpoints/puzzle.test.js
+++ test/endpoints/puzzle.test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const fs = require('fs');
 const chaiHttp = require('chai-http');
-const should = chai.should();
 const expect = chai.expect;
 const server = require('../server');
 
@@ -12,80 +11,95 @@ const TEST_HASH_UPLOAD = '67affb51d7ea5c6a9a6bd0d86e3008b3377f70c15a8ebc7d6bf6cc
 
 chai.use(chaiHttp);
 
-describe('puzzle', () => {
-  describe('POST /puzzle/check : not hash', (done) => {
+describe('POST /puzzle/check', () => {
+  describe('not hash', () => {
     // not a hash
-    it('should check the hash and return {msg: error! puzzle not found}', () => {
+    it('should check the hash and return {msg: error! puzzle not found}', (done) => {
       chai.request(server)
         .post('/puzzle/check')
         .set('content-type', 'application/json')
         .send({hash: 'i am a normal string'})
         .end((err, res) => {
-          res.should.hav.status(200);
-          res.body.should.be.json;
-          res.body.should.have.property('msg');
-          res.body.msg.should.be.equals('error! puzzle not found')
-          done();
-        });
-    });
-  });
-  describe('POST /puzzle/check : file does not exist', (done) => {
-    // file does not exists
-    it('should check the hash and return {msg: error! puzzle not found}', () => {
-      chai.request(server)
-        .post('/puzzle/check')
-        .set('content-type', 'application/json')
-        .send({hash: TEST_HASH_NOT_EXISTS})
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.json;
-          res.body.should.have.property('msg');
-          res.body.msg.should.be.equals('error! puzzle not found')
-          done();
-        });
-    });
-  });
-  describe('POST /puzzle/check : solving file exist', (done) => {
-    // good hash - solving exists
-    let file = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + TEST_HASH_SOLVING + '/solving';
-    if (!fs.existsSync(file)) {
-      fs.writeFile(file);
-    }
-    it('should check the hash and return {processing: true}', () => {
-      chai.request(server)
-        .post('/puzzle/check')
-        .set('content-type', 'application/json')
-        .send({hash: TEST_HASH_SOLVING})
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.json;
-          res.body.should.have.property('processing');
-          res.body.solving.should.be.true;
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(500);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('msg');
+          expect(res.body.msg).to.be.equals('Puzzle not found');
           done();
         });
     });
   });
 
-  describe('POST /puzzle/check : solving file does not exist', (done) => {
-    // good hash - solving does not exists
-    let file = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + process.env.TEST_HASH_SOLVED + '/solving';
-    if (fs.existsSync(file)) {
-      fs.unlink(file, (err) =>{
-        if (err) throw err;
-      });
-    }
-    it('should check the hash and return {processing: false}', () => {
+  describe('file does not exist', () => {
+    // file does not exists
+    it('should check the hash and return {msg: error! puzzle not found}', (done) => {
+      chai.request(server)
+        .post('/puzzle/check')
+        .set('content-type', 'application/json')
+        .send({hash: TEST_HASH_NOT_EXISTS})
+        .end((err, res) => {
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(500);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('msg');
+          expect(res.body.msg).to.be.equals('Puzzle not found');
+          done();
+        });
+    });
+  });
+  describe('solving file exist', () => {
+    before((done) => {
+      // good hash - solving exists
+      let file = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + TEST_HASH_SOLVING + '/solving';
+      if (!fs.existsSync(file)) {
+        fs.writeFile(file, (err) =>{
+          if (err) throw err;
+          done();
+        });
+      }
+    });
+
+    it('should check the hash and return {processing: true}', (done) => {
+      chai.request(server)
+        .post('/puzzle/check')
+        .set('content-type', 'application/json')
+        .send({hash: TEST_HASH_SOLVING})
+        .end((err, res) => {
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(200);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('processing');
+          expect(res.body.processing).to.be.true;
+          done();
+        });
+    });
+  });
+
+  describe('solving file does not exist', () => {
+    before((done) => {
+      // good hash - solving does not exists
+      let file = process.env.PRJ_DIR + process.env.UPLOAD_DIR + '/' + process.env.TEST_HASH_SOLVED + '/solving';
+      if (fs.existsSync(file)) {
+        fs.unlink(file, (err) =>{
+          if (err) throw err;
+          done();
+        });
+      }
+    });
+
+    it('should check the hash and return {processing: false}', (done) => {
       chai.request(server)
         .post('/puzzle/check')
         .set('content-type', 'application/json')
         .send({hash: process.env.TEST_HASH_SOLVED})
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.json;
-          res.body.should.have.property('processing');
-          res.body.solving.should.be.false;
-          res.body.should.have.property('pieces_data');
-          res.body.pieces_array.should.be.a('string');
+          expect(res).to.have.property('status');
+          expect(res.status).to.be.equals(200);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('processing');
+          expect(res.body.processing).to.be.false
+          expect(res.body).to.have.property('pieces_data');
+          expect(res.body.pieces_data).to.be.a('string');
           done();
         });
     });
