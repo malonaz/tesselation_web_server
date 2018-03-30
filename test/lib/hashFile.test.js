d21d5e8043dc612e17577f0131f6b8ef80c1713b
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-30 14:50:06 +0100

    fix lint errors in hashFile.test.js

--- test/lib/hashFile.test.js
+++ test/lib/hashFile.test.js
@@ -1,11 +1,10 @@
-const hashFile = require('../../src/lib/hashFile')
+const hashFile = require('../../src/lib/hashFile');
 const chai = require('chai');
 const expect = chai.expect;
 
 describe('hashFile', () => {
-
-  it('should read a file and provide a hash', (done) =>{
-    let file = "test/test.jpg";
+  it('should read a file and provide a hash', (done) => {
+    let file = 'test/test.jpg';
     hashFile(file)
       .then((hash) => {
         expect(hash).to.be.a('string');
