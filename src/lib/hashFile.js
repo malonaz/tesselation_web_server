4fd8eb783b7596261076950f05b948c4055d4e26
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-30 14:45:51 +0100

    fix lint errors in hashFile

--- src/lib/hashFile.js
+++ src/lib/hashFile.js
@@ -2,12 +2,14 @@ const crypto = require('crypto');
 const fs = require('fs');
 
 module.exports = function hashFile(filename) {
-  return new Promise((resolve, reject) => { // async process, need promise to ensure that the file read and hash are completed before returning the hash
+  return new Promise((resolve) => {
+    // async process, need promise to ensure that the file read and hash are
+    // completed before returning the hash
     const hash = crypto.createHash('sha256').setEncoding('hex');
     const input = fs.createReadStream(filename);
     input
       .pipe(hash)
-      .on('finish', function() {
+      .on('finish', () => {
         resolve(this.read());
       });
   });
