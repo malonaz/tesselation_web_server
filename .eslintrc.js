9b6095012cb189c3566030b0ce07dc118cd50cfb
Sam-Mauris Yong <mauris@hotmail.sg>
2018-03-30 17:35:25 +0100

    update to turn off "no-unused-expressions"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true,
-    "mocha": true
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true
   },
-  "extends": "airbnb-base/legacy",
-  "rules": {
-    "no-console": ["warn", { "allow": ["log"] }],
-    "no-underscore-dangle": "off"
+  'extends': 'airbnb-base/legacy',
+  'rules': {
+    'no-console': ['warn', { 'allow': ['log'] }],
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': 'off',
   },
-  "parserOptions": {
-    "sourceType": "module"
+  'parserOptions': {
+    'sourceType': 'module'
   }
 };
