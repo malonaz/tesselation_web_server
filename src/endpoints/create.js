4047cd43e5e44d1d62def5423ac63e936ad86dc3
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-05-14 15:52:15 +0100

    sadas

--- src/endpoints/create.js
+++ src/endpoints/create.js
@@ -62,9 +62,11 @@ function generatePuzzle(puzzleSize, callback){
   const exec1 = process.env.GENERATOR_PATH;
   let dir = './web/' + tempF;
   let cmd = '"' + exec1 + '" "' + puzzleSize + '"';
+
   console.log(dir);
   console.log(cmd);
   childProcess.exec(cmd, { cwd: dir }, (error, stdout, stderr) => {
+    console.log('exec pb');
     console.log(error);
     console.log(stderr);
     console.log(stdout);
